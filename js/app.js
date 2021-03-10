0ce4724af58cedc8c063b07438ed2a3ec28339e0
Timothy Viccari <tjvicc84@gmail.com>
2021-03-10 12:28:35 -0500

    Added lowercase to all statements add console log

--- js/app.js
+++ js/app.js
@@ -1,70 +1,62 @@
 'use strict';
 
-let username = prompt('Hello, what is your name?')
+let userName = prompt('Hello, what is your name?')
 //alert('Hello ' + username);
 //console.log('You are' + username);
 
-let question1 = prompt(`Ok ${username}, was Tim born in Florida?`);
+let question1 = prompt(`Ok ${userName}, was Tim born in Florida?`);
 switch (question1.toLowerCase()) {
     case 'yes':
         alert('That is correct')
+        //console.log('That is correct')
         break;
     case 'y':
         alert('That is correct')
+        //console.log('That is correct')
         break;
     case 'no':
         alert('That is incorrect')
+        //console.log('That is incorrect')
         break; 
     case 'n':
         alert('That is incorrect')
+        //console.log('That is incorrect')
         break;
   default:
         alert('It\'s a yes or no question - you fail');
         }
 
-let question2 = prompt(`Ok ${username}, was Tim in the Marines?`);
-switch (question2) {
+let question2 = prompt(`Ok ${userName}, was Tim in the Marines?`);
+switch (question2.toLowerCase()) {
     case 'yes':
         alert('That is incorrect')
+        //console.log('That is incorrect')
         break;
     case 'y':
         alert('That is incorrect')
+        //console.log('That is incorrect')
         break;
     case 'no':
         alert('That is correct')
+        //console.log('That is correct')
         break; 
     case 'n':
         alert('That is correct')
+        //console.log('That is correct')
         break;
   default:
         alert('It\'s a yes or no question - you fail');
         }
 
-let question3 = prompt(`Ok ${username}, Did Tim go to university in Maine?`);
-switch (question3) {
+let question3 = prompt(`Ok ${userName}, Did Tim go to university in Maine?`);
+switch (question3.toLowerCase()) {
     case 'yes':
         alert('That is incorrect')
+        //console.log('That is incorrect')
         break;
     case 'y':
         alert('That is incorrect')
-        break;
-    case 'no':
-        alert('That is correct')
-        break; 
-    case 'n':
-        alert('That is correct')
-        break;
-  default:
-        alert('It\'s a yes or no question - you fail');
-        }
-
-let question4 = prompt(`Ok ${username}, did Tim meet his wife at work?`);
-switch (question4) {
-    case 'yes':
-        alert('That is incorrect')
-        break;
-    case 'y':
-        alert('That is incorrect')
+        //console.log('That is incorrect')
         break;
     case 'no':
         alert('That is correct')
@@ -76,19 +68,45 @@ switch (question4) {
         alert('It\'s a yes or no question - you fail');
         }
 
-let question5 = prompt(`Ok ${username}, did Tim study finance?`);
-switch (question5) {
+let question4 = prompt(`Ok ${userName}, did Tim meet his wife at work?`);
+switch (question4.toLowerCase()) {
+    case 'yes':
+        alert('That is incorrect')
+        //console.log('That is incorrect')
+        break;
+    case 'y':
+        alert('That is incorrect')
+        //console.log('That is incorrect')
+        break;
+    case 'no':
+        alert('That is correct')
+        //console.log('That is correct')
+        break; 
+    case 'n':
+        alert('That is correct')
+        //console.log('That is correct')
+        break;
+  default:
+        alert('It\'s a yes or no question - you fail');
+        }
+
+let question5 = prompt(`Ok ${userName}, did Tim study finance?`);
+switch (question5.toLowerCase()) {
     case 'yes':
         alert('That is correct')
+        //console.log('That is correct')
         break;
     case 'y':
         alert('That is correct')
+        //console.log('That is correct')
         break;
     case 'no':
         alert('That is incorrect')
+        //console.log('That is incorrect')
         break; 
     case 'n':
         alert('That is incorrect')
+        //console.log('That is incorrect')
         break;
   default:
         alert('It\'s a yes or no question - you fail');
