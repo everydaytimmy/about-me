ae4b193a59ee15a5c998f42ca0ec84ec9d39bdbb
Timothy Viccari <timothyviccari@Timothys-MacBook-Pro.local>
2021-03-09 20:16:30 -0500

    WIP

---
+++ js/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+//let username = prompt('Hello, what is your name?')
+//alert('Hello ' + username);
+//console.log('You are' + username);
+
+let question1 = prompt('Ok ' + username + ', was Tim born in Florida?');
+switch (question1) {
+    case 'yes':
+        alert('That is correct')
+        break;
+    case 'y':
+        alert('That is correct')
+        break;
+    case 'no':
+        alert('That is incorrect')
+        break; 
+    case 'n':
+        alert('That is incorrect')
+        break;
+  default:
+        alert('It\'s a yes or no question - you fail');
+        }
+
+// let question2 = prompt('Ok ' + username + ', was Tim in the Marines?');
+// switch (question2) {
+//     case 'yes':
+//         alert('That is incorrect')
+//         break;
+//     case 'y':
+//         alert('That is incorrect')
+//         break;
+//     case 'no':
+//         alert('That is correct')
+//         break; 
+//     case 'n':
+//         alert('That is correct')
+//         break;
+//   default:
+//         alert('It\'s a yes or no question - you fail');
+//         }
+
+        
+
+//console.log(username + ' from ' + response2);
